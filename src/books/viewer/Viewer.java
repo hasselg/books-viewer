6f62c0212e47095cb617e297d2e98da6ce1b58ba
Greg Hasseler <ghasseler@gmail.com>
2014-04-20 12:20:00 -0400

    Misc. code cleanup.
    Improved code readability.

--- src/books/viewer/Viewer.java
+++ src/books/viewer/Viewer.java
@@ -21,20 +21,25 @@ public class Viewer extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.setSecurityManager(null);
         launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Books!");
-        
-        WebView browser = new WebView();
-        WebEngine webEngine = browser.getEngine();
-        webEngine.load("http://localhost:8080/books/library/");
+        java.net.CookieHandler.setDefault(null);
         
         StackPane root = new StackPane();
+                
+        WebView browser = new WebView();
+        WebEngine webEngine = browser.getEngine();
+        webEngine.load("http://localhost:8080/books/library");
+
         root.getChildren().add(browser);
-        primaryStage.setScene(new Scene(root, 800, 600));
+        
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("Books!");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
